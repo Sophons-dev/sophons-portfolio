4513d8faabac8335baf0ef0ea3598d6413cee755
martin <meatole.cs@gmail.com>
2025-08-02 22:19:14 +0800

    fix: SEO metadata

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,10 +3,16 @@ import { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://sophons.pro',
+      url: 'https://sophons.tech',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
     },
+    {
+      url: 'https://www.sophons.tech',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    }
   ];
 }
